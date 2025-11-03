9b27064cf707012f1f07f0d908a20cab4baf59ac
gcnaveen <naveengc99@gmail.com>
2025-11-03 22:47:03 +0530

    org changes

--- Modals/ClientsBookings/index.js
+++ Modals/ClientsBookings/index.js
@@ -1,3 +1,43 @@
+// const mongoose = require("mongoose");
+
+// const advanceSchema = new mongoose.Schema({
+//   advanceNumber: { type: Number, required: true },
+//   expectedAmount: { type: Number, required: true },
+//   receivedAmount: { type: Number, default: 0 },
+//   receivedDate: { type: Date },
+
+//   remarks: {
+//     accounts: { type: String, default: "" },
+//     owner: { type: String, default: "" },
+//     approver: { type: String, default: "" }
+//   },
+
+//   updatedBy: {
+//     accounts: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null },
+//     owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null },
+//     approver: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null }
+//   },
+
+//   updatedAt: {
+//     accounts: { type: Date },
+//     owner: { type: Date },
+//     approver: { type: Date }
+//   }
+// }, { _id: false });
+
+// const eventSchema = new mongoose.Schema({
+//   clientName: { type: String, required: true },
+//   eventDate: { type: Date, required: true },
+//   venueLocation: { type: String, required: true },
+//   agreedAmount: { type: Number, required: true },
+
+//   advances: [advanceSchema]
+
+// }, { timestamps: true });
+
+// const Event = mongoose.model("Event", eventSchema);
+
+// module.exports = Event;
 const mongoose = require("mongoose");
 
 const advanceSchema = new mongoose.Schema({
@@ -27,12 +67,13 @@ const advanceSchema = new mongoose.Schema({
 
 const eventSchema = new mongoose.Schema({
   clientName: { type: String, required: true },
+  brideName: { type: String },          // Added brideName
+  groomName: { type: String },          // Added groomName
   eventDate: { type: Date, required: true },
   venueLocation: { type: String, required: true },
   agreedAmount: { type: Number, required: true },
 
   advances: [advanceSchema]
-
 }, { timestamps: true });
 
 const Event = mongoose.model("Event", eventSchema);
--- Routes/ClientsBookings/index.js
+++ Routes/ClientsBookings/index.js
@@ -1,3 +1,23 @@
+// const express = require("express");
+// const router = express.Router();
+// const eventController = require("../../Controlers/ClientsBookings");
+
+// // Create new event
+// router.post("/", eventController.createEvent);
+
+// // Update advance received amount and remarks by role
+// router.patch("/:eventId/advances/:advanceNumber", eventController.updateAdvance);
+
+// // Get event details
+// router.get("/:eventId", eventController.getEvent);
+
+// router.get("/", eventController.getAllEvents);
+
+// router.put("/:eventId/edit", eventController.editEventExceptReceivedAmount);
+
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 const eventController = require("../../Controlers/ClientsBookings");
@@ -8,12 +28,13 @@ router.post("/", eventController.createEvent);
 // Update advance received amount and remarks by role
 router.patch("/:eventId/advances/:advanceNumber", eventController.updateAdvance);
 
-// Get event details
+// Get event details by ID
 router.get("/:eventId", eventController.getEvent);
 
+// Get all events with pagination
 router.get("/", eventController.getAllEvents);
 
+// Edit event details except receivedAmount in advances
 router.put("/:eventId/edit", eventController.editEventExceptReceivedAmount);
-
 
 module.exports = router;
