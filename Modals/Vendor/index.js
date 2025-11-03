dab73da6d8fb23aa82b7f08610cac08f0b232a10
gcnaveen <naveengc99@gmail.com>
2025-11-03 22:32:10 +0530

    org changes

--- Modals/Vendor/index.js
+++ Modals/Vendor/index.js
@@ -1,102 +1,271 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+// const ObjectId = Schema.Types.ObjectId;
+
+// const vendorSchema = new Schema(
+//     {
+//         // vendor_id: {
+//         //     type: Number,
+//         //     required: true,
+//         //     default: 0
+//         // },
+//         name: {
+//             type: String,
+//             required: true
+//         },
+        
+//         email: {
+//             type: String,
+//             required: false
+//         },
+        
+//         address: {
+//             type: String,
+//             required: true
+//         },
+        
+//         city: {
+//             type: String,
+//             required: true
+//         },
+        
+//         state: {
+//             type: String,
+//             required: true
+//         },
+        
+//         zip: {
+//             type: String,
+//             required: true
+//         },
+//         persone_name: {
+//             type: String,
+//             required: true
+//         },
+        
+//         country: {
+//             type: String,
+//             required: true
+//         },
+//         vendor_belongs_to: {
+//             type: ObjectId,
+//             ref: 'user',
+//             required: true
+//         },
+//         vendor_category: {
+//             type: String,
+//             enum: ['INDIVIDUAL', 'HUF','COMPANY','FIRM'],
+//             required: true
+//         },
+//         vendor_type: {
+//             type: String,
+//             enum: ['MATERIAL', 'LABOUR','COMPOSITE',"EXPENSES"],
+//             required: true
+//         },
+//         vendor_phone: {
+//             type: String,
+//             required: true
+//         },
+//         vendor_status: {
+//             type: String,
+//             enum: ['ACTIVE', 'INACTIVE'],
+//             default: 'ACTIVE',
+//             required: false
+//         },
+//        gst_number: {
+//             type: String,
+//             required: false
+//         },
+//         pan_number: {
+//             type: String,
+//             required: false
+//         },
+//         msmed_number: {
+//             type: String,
+//             required: false
+//         },
+    
+//     },
+//     {
+//         timestamps: true
+//     }
+// );
+
+// vendorSchema.set("toJSON", {
+//     transform: (doc, ret, options) => {
+//         ret.id = ret._id;
+//         delete ret._id;
+//         delete ret.__v;
+//     }
+// });
+
+// module.exports = mongoose.model('vendor', vendorSchema);
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
 const vendorSchema = new Schema(
-    {
-        // vendor_id: {
-        //     type: Number,
-        //     required: true,
-        //     default: 0
-        // },
-        name: {
-            type: String,
-            required: true
-        },
-        
-        email: {
-            type: String,
-            required: false
-        },
-        
-        address: {
-            type: String,
-            required: true
-        },
-        
-        city: {
-            type: String,
-            required: true
-        },
-        
-        state: {
-            type: String,
-            required: true
-        },
-        
-        zip: {
-            type: String,
-            required: true
-        },
-        persone_name: {
-            type: String,
-            required: true
-        },
-        
-        country: {
-            type: String,
-            required: true
-        },
-        vendor_belongs_to: {
-            type: ObjectId,
-            ref: 'user',
-            required: true
-        },
-        vendor_category: {
-            type: String,
-            enum: ['INDIVIDUAL', 'HUF','COMPANY','FIRM'],
-            required: true
-        },
-        vendor_type: {
-            type: String,
-            enum: ['MATERIAL', 'LABOUR','COMPOSITE',"EXPENSES"],
-            required: true
-        },
-        vendor_phone: {
-            type: String,
-            required: true
-        },
-        vendor_status: {
-            type: String,
-            enum: ['ACTIVE', 'INACTIVE'],
-            default: 'ACTIVE',
-            required: false
-        },
-       gst_number: {
-            type: String,
-            required: false
-        },
-        pan_number: {
-            type: String,
-            required: false
-        },
-        msmed_number: {
-            type: String,
-            required: false
-        },
-    
+  {
+    name: {
+      type: String,
+      required: true
     },
-    {
-        timestamps: true
+    person_category: {
+      type: String,
+      enum: ['INDIVIDUAL', 'HUF', 'COMPANY', 'FIRM'],
+      required: true
+    },
+    company_name: {
+      type: String,
+      required: function () {
+        return this.person_category === 'COMPANY';
+      }
+    },
+    temp_address_1: {
+      type: String,
+      required: false
+    },
+    temp_city: {
+      type: String,
+      required: false
+    },
+    temp_pin: {
+      type: String,
+      required: false
+    },
+    temp_state: {
+      type: String,
+      required: false
+    },
+    temp_country: {
+      type: String,
+      required: false
+    },
+    perm_address_1: {
+      type: String,
+      required: false
+    },
+    perm_address_2: {
+      type: String,
+      required: false
+    },
+    perm_city: {
+      type: String,
+      required: false
+    },
+    perm_pin: {
+      type: String,
+      required: false
+    },
+    perm_state: {
+      type: String,
+      required: false
+    },
+    perm_country: {
+      type: String,
+      required: false
+    },
+    cont_person: {
+      type: String,
+      required: false
+    },
+    designation: {
+      type: String,
+      required: false
+    },
+    mobile_no: {
+      type: String,
+      required: false
+    },
+    alt_mobile_no: {
+      type: String,
+      required: false
+    },
+    email: {
+      type: String,
+      required: false
+    },
+    vendor_type: {
+      type: String,
+      enum: ['MATERIAL', 'LABOUR', 'COMPOSITE', 'EXPENSES'],
+      required: true
+    },
+    gst_no: {
+      type: String,
+      required: false
+    },
+    msmed_no: {
+      type: String,
+      required: false
+    },
+    pan_no: {
+      type: String,
+      required: false
+    },
+    bank_name: {
+      type: String,
+      required: false
+    },
+    beneficiary_name: {
+      type: String,
+      required: false
+    },
+    bank_address_1: {
+      type: String,
+      required: false
+    },
+    bank_address_2: {
+      type: String,
+      required: false
+    },
+    bank_pin: {
+      type: String,
+      required: false
+    },
+    account_number: {
+      type: String,
+      required: false
+    },
+    ifscode: {
+      type: String,
+      required: false
+    },
+    branch: {
+      type: String,
+      required: false
+    },
+    payment_terms: {
+      type: String,
+      required: false
+    },
+    tds_details: {
+      type: String,
+      required: false
+    },
+    vendor_status: {
+      type: String,
+      enum: ['ACTIVE', 'INACTIVE'],
+      default: 'ACTIVE',
+      required: false
+    },
+    vendor_belongs_to: {
+      type: ObjectId,
+      ref: 'user',
+      required: true
     }
+  },
+  {
+    timestamps: true
+  }
 );
 
 vendorSchema.set("toJSON", {
-    transform: (doc, ret, options) => {
-        ret.id = ret._id;
-        delete ret._id;
-        delete ret.__v;
-    }
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
 });
 
 module.exports = mongoose.model('vendor', vendorSchema);
--- Routes/Vendor/index.js
+++ Routes/Vendor/index.js
@@ -1,3 +1,17 @@
+// const express = require('express');
+// const router = express.Router();
+// const vendorController = require('../../Controlers/Vendor');
+// const auth = require('../../authentication/is-auth'); 
+
+// router.post('/', auth, vendorController.createVendor);
+// router.get('/', auth, vendorController.getVendors);
+// router.get('/all', vendorController.getAllVendors);
+// router.get('/:id', auth, vendorController.getVendorById);
+// router.put('/:id', auth, vendorController.updateVendor);
+// router.get('/department/:id', auth, vendorController.getVendorsByDepartmentId);
+// router.delete('/:id', auth, vendorController.deleteVendor);
+
+// module.exports = router;
 const express = require('express');
 const router = express.Router();
 const vendorController = require('../../Controlers/Vendor');
