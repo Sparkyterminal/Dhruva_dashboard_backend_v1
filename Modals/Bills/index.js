47c5e948ad168360f904efabce18e25dba66d02c
gcnaveen <naveengc99@gmail.com>
2025-11-03 21:26:17 +0530

    org changes

--- Modals/Bills/index.js
+++ Modals/Bills/index.js
@@ -1,4 +1,36 @@
+// const mongoose = require('mongoose');
+
+// const billSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//   },
+//   emiDate: {
+//     type: Date,
+//     required: true,
+//   },
+//   amount: {
+//     type: Number,
+//     required: true,
+//     min: 0,
+//   }
+// }, {
+//   timestamps: true, 
+// });
+
+// const Bill = mongoose.model('Bill', billSchema);
+
+// module.exports = Bill;
+
+
 const mongoose = require('mongoose');
+
+const emiStatusSchema = new mongoose.Schema({
+  month: { type: Number, required: true },  // 1-12
+  year: { type: Number, required: true },   // full year e.g. 2025
+  paid: { type: Boolean, default: false }
+}, {_id:false});
 
 const billSchema = new mongoose.Schema({
   name: {
@@ -6,7 +38,7 @@ const billSchema = new mongoose.Schema({
     required: true,
     trim: true,
   },
-  emiDate: {
+  emiDate: {  // The date of the EMI (day-of-month)
     type: Date,
     required: true,
   },
@@ -14,9 +46,18 @@ const billSchema = new mongoose.Schema({
     type: Number,
     required: true,
     min: 0,
-  }
+  },
+  emiStatus: [emiStatusSchema]  // Track status per month/year
 }, {
-  timestamps: true, 
+  timestamps: true,
+});
+
+billSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
 });
 
 const Bill = mongoose.model('Bill', billSchema);
--- Routes/Bills/index.js
+++ Routes/Bills/index.js
@@ -1,3 +1,15 @@
+// const express = require('express');
+// const router = express.Router();
+// const billController = require('../../Controlers/Bills');
+
+// router.post('/', billController.createBill);
+// router.get('/', billController.getBills);
+// router.get('/:id', billController.getBillById);
+// router.put('/:id', billController.updateBill);
+// router.delete('/:id', billController.deleteBill);
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
 const billController = require('../../Controlers/Bills');
@@ -7,5 +19,12 @@ router.get('/', billController.getBills);
 router.get('/:id', billController.getBillById);
 router.put('/:id', billController.updateBill);
 router.delete('/:id', billController.deleteBill);
+
+module.exports = router;
+
+
+// New routes for EMI status
+// router.get('/emi/status', billController.getEMIStatus);
+// router.put('/emi/status/:id', billController.updateEMIPaymentStatus);
 
 module.exports = router;
