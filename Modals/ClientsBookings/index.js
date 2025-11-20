297eb0bfb104385b8fe746542c37cb8db6bd3be8
gcnaveen <naveengc99@gmail.com>
2025-11-20 15:56:43 +0530

    checklist

--- Modals/ClientsBookings/index.js
+++ Modals/ClientsBookings/index.js
@@ -1,50 +1,10 @@
-// const mongoose = require("mongoose");
-
-// const advanceSchema = new mongoose.Schema({
-//   advanceNumber: { type: Number, required: true },
-//   expectedAmount: { type: Number, required: true },
-//   receivedAmount: { type: Number, default: 0 },
-//   receivedDate: { type: Date },
-
-//   remarks: {
-//     accounts: { type: String, default: "" },
-//     owner: { type: String, default: "" },
-//     approver: { type: String, default: "" }
-//   },
-
-//   updatedBy: {
-//     accounts: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null },
-//     owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null },
-//     approver: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null }
-//   },
-
-//   updatedAt: {
-//     accounts: { type: Date },
-//     owner: { type: Date },
-//     approver: { type: Date }
-//   }
-// }, { _id: false });
-
-// const eventSchema = new mongoose.Schema({
-//   clientName: { type: String, required: true },
-//   eventDate: { type: Date, required: true },
-//   venueLocation: { type: String, required: true },
-//   agreedAmount: { type: Number, required: true },
-
-//   advances: [advanceSchema]
-
-// }, { timestamps: true });
-
-// const Event = mongoose.model("Event", eventSchema);
-
-// module.exports = Event;
 
 const mongoose = require("mongoose");
 
 const advanceSchema = new mongoose.Schema({
   advanceNumber: { type: Number, required: true },
   expectedAmount: { type: Number, required: true },
-  advanceDate: { type: Date, required: true },  // Added advanceDate
+  advanceDate: { type: Date, required: true },
 
   receivedAmount: { type: Number, default: 0 },
   receivedDate: { type: Date },
@@ -68,15 +28,23 @@ const advanceSchema = new mongoose.Schema({
   }
 }, { _id: false });
 
-const eventSchema = new mongoose.Schema({
-  clientName: { type: String, required: true },
-  brideName: { type: String },
-  groomName: { type: String },
+const eventTypeSchema = new mongoose.Schema({
+  eventType: { type: String, required: true },
   eventDate: { type: Date, required: true },
   venueLocation: { type: String, required: true },
   agreedAmount: { type: Number, required: true },
+  advances: { type: [advanceSchema], default: [] }
+}, { _id: false });
 
-  advances: [advanceSchema]
+const eventSchema = new mongoose.Schema({
+  eventName: { type: String, required: true },
+  eventTypes: { type: [eventTypeSchema], required: true, default: [] },
+  clientName: { type: String, required: true },
+  brideName: { type: String },
+  groomName: { type: String },
+  contactNumber: { type: String, required: true },
+  altContactNumber: { type: String },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
 }, { timestamps: true });
 
 const Event = mongoose.model("Event", eventSchema);
