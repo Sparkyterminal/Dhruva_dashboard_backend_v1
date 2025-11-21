61c58ff257ceb1cff121b8a40f9bfa1e714881f5
gcnaveen <naveengc99@gmail.com>
2025-11-21 22:55:04 +0530

    error

--- Routes/Vendor/index.js
+++ Routes/Vendor/index.js
@@ -18,12 +18,12 @@ const vendorController = require('../../Controlers/Vendor');
 const auth = require('../../authentication/is-auth'); 
 
 router.post('/', auth, vendorController.createVendor);
-router.get('/', auth, vendorController.getVendors);
 router.get('/all', vendorController.getAllVendors);
 router.get('/list', auth, vendorController.getAllVendorsList);
+router.get('/department/:id', auth, vendorController.getVendorsByDepartmentId);
+router.get('/', auth, vendorController.getVendors);
 router.get('/:id', auth, vendorController.getVendorById);
 router.put('/:id', auth, vendorController.updateVendor);
-router.get('/department/:id', auth, vendorController.getVendorsByDepartmentId);
 router.delete('/:id', auth, vendorController.deleteVendor);
 
 module.exports = router;
