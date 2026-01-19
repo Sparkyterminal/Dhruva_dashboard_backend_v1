214635e9821e7ccaf3fa510ad073f48f176912b0
gcnaveen <naveengc99@gmail.com>
2026-01-19 12:38:18 +0530

    bills changes

--- Routes/Bills/index.js
+++ Routes/Bills/index.js
@@ -1,3 +1,5 @@
+
+
 // const express = require('express');
 // const router = express.Router();
 // const billController = require('../../Controlers/Bills');
@@ -10,21 +12,24 @@
 
 // module.exports = router;
 
+
+
+
+// module.exports = router;
 const express = require('express');
 const router = express.Router();
 const billController = require('../../Controlers/Bills');
 
+// Basic CRUD routes
 router.post('/', billController.createBill);
 router.get('/', billController.getBills);
 router.get('/:id', billController.getBillById);
 router.put('/:id', billController.updateBill);
 router.delete('/:id', billController.deleteBill);
 
-module.exports = router;
-
-
-// New routes for EMI status
-// router.get('/emi/status', billController.getEMIStatus);
-// router.put('/emi/status/:id', billController.updateEMIPaymentStatus);
+// EMI Status routes
+router.get('/emi/status', billController.getEMIStatus);
+router.put('/emi/status/:id', billController.updateEMIPaymentStatus);
+router.put('/emi/bulk/:id', billController.bulkUpdateEMIAmounts);
 
 module.exports = router;
